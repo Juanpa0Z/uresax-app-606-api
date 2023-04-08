ad676a8a2db25bd14e88d861257c81cf845a40ee
juanpa0z <juanpacode@gmail.com>
2023-04-08 16:22:57 -0400

    upload

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const app = express();
+
+app.get('/',(req,res)=> res.send('Working...'))
+
+app.get('/users',(req,res)=> res.status(200).json([]))
+
+app.get('/purchases',(req,res)=> res.status(200).json([
+     {
+        id:1,
+        rnc:'101675489',
+        tax_payer_name:'URENA LORENZO & ASOCIADOS SRL',
+        ncfType:1,
+        ncf:'B0100000001',
+        ncfModifedType:null,
+        ncfModifed:null,
+        total:2500,
+        tax:550.45,
+        created_at:new Date().toDateString()
+     }
+]))
+
